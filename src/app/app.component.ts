9070c582f505f5c7001593e6975d589390618442
Justin <Justin@grandcircus.co>
2024-07-05 00:53:52 -0400

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Post } from './models/post';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo';
+  constructor(private http: HttpClient){}
+  allPosts:Post[] = [];
+  formPost:Post = {} as Post;
+
+  ngOnInit(){
+    this.getPosts();
+  }
+
+  getPosts(){
+    this.http.get<Post[]>("https://webapplication220240705003824.azurewebsites.net/api/post")
+    .subscribe((response:Post[])=> {
+      this.allPosts = response;
+    });
+  }
+
+  addPost(){
+    this.http.post<void>("https://webapplication220240705003824.azurewebsites.net/api/post", this.formPost)
+    .subscribe(() =>{
+      this.getPosts();
+    })
+  }
 }
